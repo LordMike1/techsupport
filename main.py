532641218084df14445194709341fcad6e1fb04d
LordMike <michalwyka2004@gmail.com>
2021-03-04 20:24:21 +0000

    + ?cantjoin command which returns 3 steps: check version, restart the game and launcher, and java question.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('?cantjoin'):
+        await message.channel.send('Follow these steps to fix most of the common problems when joining!\n\n\t **Step 1:** Ensure you are on Java 1.16.4/5. You can join from either 1.16.4 or .5\n\n\t **Step 2:** Restart your game and launcher.\n\n\t **Step 3:** Are you joining from the Java version of Minecraft? You cannot join jonahwill.org from bedrock or Pocket Edition.\n\n If these steps have not worked, open a ticket and we will get back to you asap.')
 
 client.run(os.getenv('TOKEN'))
