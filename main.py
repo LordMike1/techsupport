5fb850370d56cb6f98e93221eb71f5ce763146d7
LordMike <michalwyka2004@gmail.com>
2021-03-04 20:24:56 +0000

    + ?ip command which returns the IP in an inline code block.

--- main.py
+++ main.py
@@ -15,4 +15,7 @@ async def on_message(message):
     if message.content.startswith('?cantjoin'):
         await message.channel.send('Follow these steps to fix most of the common problems when joining!\n\n\t **Step 1:** Ensure you are on Java 1.16.4/5. You can join from either 1.16.4 or .5\n\n\t **Step 2:** Restart your game and launcher.\n\n\t **Step 3:** Are you joining from the Java version of Minecraft? You cannot join jonahwill.org from bedrock or Pocket Edition.\n\n If these steps have not worked, open a ticket and we will get back to you asap.')
 
+    if message.content.startswith('?ip'):
+        await message.channel.send('To join the server use this IP:\n`jonahwill.org`')
+
 client.run(os.getenv('TOKEN'))
